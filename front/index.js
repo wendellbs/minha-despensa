a13e7f1f3806ed3b32b0ea6c02e9247e01ed9ccf
Rafael Eyng <rafaeleyng@gmail.com>
2016-09-01 13:07:06 -0300

    filter products in 2 groups

--- front/index.js
+++ front/index.js
@@ -1,18 +1,57 @@
 angular.module('app', [])
   .controller('ListCtrl', function() {
     var ctrl = this
+
+    today = moment().locale('pt')
+
     ctrl.listProduct = [
       {
-        quantity: 10,
-        name: 'Quejo',
-        purchaseDate: new Date(),
-        expectedDuration: 5,
+        quantity: 8,
+        name: 'Banana',
+        purchaseDate: moment().locale('pt').subtract(8, 'days'),
+        expectedDuration: 14,
       },
       {
         quantity: 5,
         name: 'Pão',
-        purchaseDate: new Date(),
+        purchaseDate: moment().locale('pt').subtract(8, 'days'),
         expectedDuration: 7,
-      }
+      },
+      {
+        quantity: 6,
+        name: 'Presunto',
+        purchaseDate: moment().locale('pt').subtract(1, 'days'),
+        expectedDuration: 4,
+      },
+      {
+        quantity: 10,
+        name: 'Queijo',
+        purchaseDate: moment().locale('pt').subtract(1, 'days'),
+        expectedDuration: 5,
+      },
+      {
+        quantity: 10,
+        name: 'Maçã',
+        purchaseDate: moment().locale('pt').subtract(1, 'days'),
+        expectedDuration: 20,
+      },
     ]
+
+    var isFresh = function(p) {
+      var diffInDays = today.diff(p.purchaseDate, 'days') + 1
+      console.log('diff', diffInDays, p.expectedDuration)
+      return diffInDays < p.expectedDuration
+    }
+
+    ctrl.getListExpiringProduct = function() {
+      return ctrl.listProduct.filter(function(p) {
+        return !isFresh(p)
+      })
+    }
+
+    ctrl.getListFreshProduct = function() {
+      return ctrl.listProduct.filter(function(p) {
+        return isFresh(p)
+      })
+    }
   })
